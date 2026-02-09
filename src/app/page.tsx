12c00c3ba00b5561a6098039d1d702f9e4bbc4a0
ÓscarGE <geoscar1100@gmail.com>
2026-02-09 01:04:14 +0100

    added api logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+import useCoins from './hooks/useCoins';
+
 export default function Home() {
+  const { data: coins, isError, isPending } = useCoins();
+
   return (
     <>
       <h1 className="">Testing</h1>
